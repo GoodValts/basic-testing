a0a64b2b8f8cb87a3e086380a59dc7151d8d6c61
Martynjuk Ivan <martiniukgomel@gmail.com>
2024-02-12 23:58:25 +0300

    feat: add snapshot tests Mon, Feb 12, 2024 11:58:25 PM

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,38 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
+
+type LinkedListNode<T> = {
+  value: T | null;
+  next: LinkedListNode<T> | null;
+};
 
 describe('generateLinkedList', () => {
+  const elements: number[] = [];
+  const expectedLinkedList: LinkedListNode<number> = {
+    value: null,
+    next: null,
+  };
+
+  const checkIsElement = (obj: LinkedListNode<number>, number: number) => {
+    if (!obj.value) {
+      obj.value = number;
+      obj.next = { value: null, next: null };
+    } else {
+      if (obj.next) checkIsElement(obj.next, number);
+    }
+  };
+
+  for (let i = 0; i < 100; i++) {
+    elements.push(i + 1);
+    checkIsElement(expectedLinkedList, i + 1);
+  }
+
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    expect(generateLinkedList(elements)).toStrictEqual(expectedLinkedList);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    expect(generateLinkedList(elements)).toMatchSnapshot();
   });
 });
